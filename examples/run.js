813d3a45a5c5bc0279a931ab0e34a41ed0f126dd
Holger Will <h.will@klimapartner.de>
2016-08-27 09:12:56 +0200

    exapmle for changing listeners

--- examples/run.js
+++ examples/run.js
@@ -9,18 +9,36 @@ var lightscene = new Lightscene(enocean)
 enocean.listen("/dev/ttyUSB0");
 enocean.on("ready", async (function(){
   var st="Test  : w 1000, s 1 on   ,w 1000 , d 4 50 , w 1000 ,  s 1 off,w 1000 , d 4 40 01    "
-  var s=  {
-    "name":"Test",
-    "desc":"Test",
-    "run":[{"type":"switch","ch":1,"value":"off"},{"type":"dimmer","ch":4,"value":100,"speed":"ff"}]
+  var s= {
+      "name": "Dinner",
+      "des": "Dinner",
+      "listener": [
+          {
+              "id": "002a1d4d",
+              "button": "B1"
+          }
+      ],
+      "run": [
+          {
+              "type": "switch",
+              "ch": 1,
+              "value": "on"
+          },
+          {
+              "type": "dimmer",
+              "ch": 4,
+              "value": 50,
+              "speed": "50"
+          }
+      ]
   }
   //console.log("Add JSON")
   //await(lightscene.addOrEdit(s))
   //console.log("Edit String")
-  await(lightscene.addOrEdit(st))
-  console.log("Execute sved Scene named in CLI")
+  await(lightscene.addOrEdit(s))
+  console.log("Execute saved Scene named in CLI")
   await(lightscene.execute(process.argv[2]))
   console.log("execute save Scene by name")
-  await(lightscene.execute("Test"))
+  await(lightscene.execute("AntiDinner"))
   enocean.close()
 }))
